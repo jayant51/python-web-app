3ec5d59856cb130f70803e758df067bfb5d8730d
jayc51 <jayant.kulkarni@ibm.com>
2024-04-01 23:12:47 -0700

    updated

--- app.py
+++ app.py
@@ -419,12 +419,12 @@ def keys(d, c=[]):
 
 
 def create_connection():
-    """create a database connection to a database that resides
-    in the memory
-    """
+    print("create a database connection to a database that resides in the memory")
+
     conn = None
     try:
-        conn = sqlite3.connect(":memory:")
+        # conn = sqlite3.connect(":memory:")
+        conn = sqlite3.connect("supplychain.db")
         print(sqlite3.version)
     except Error as e:
         print(e)
